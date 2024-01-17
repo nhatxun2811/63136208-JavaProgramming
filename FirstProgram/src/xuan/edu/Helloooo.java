341c58634c62969e19e8eb8430ea2234f0df530f
nhatxun2811 <xxuan0621@gmail.com>
2024-01-17 20:04:29 +0700

    First Example
    Test Github

---
+++ FirstProgram/src/xuan/edu/Helloooo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package xuan.edu;
+
+/**
+ * 
+ */
+public class Helloooo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// 
+		System.out.println("Hé lô e ri bo đi");
+	}
+
+}
